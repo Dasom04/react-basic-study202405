0e0624b6b83e2ff37e2469c2006403eb8c090110
dasom <slz_952@naver.com>
2024-05-01 16:13:21 +0900

    1. React 초기설정 완료 및 Component 개념확인

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import NoName from './NoName';
 
 function App() {
+
+  const $h2 = <h2>반가워요~~!</h2>; // 바자 스크립트 문법임. 생긴게 html이랑 비슷할 뿐
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <NoName />
+      <div className='App'>
+        <h1>메롱메롱~~!!!</h1>
+        {$h2}
+      </div>
+      <div className='noname'>
+        <input type='text' />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          오늘은 5월1일 수요일 입니디. <br />
+          오후 3시 30분 입니다.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </>
   );
 }
 
