6f1026310a7a9022385656df58330b146d5cbb88
Dasom04 <slz_952@naver.com>
2024-05-02 16:56:48 +0900

    9. 동적리스트 렌더링, 연도별 필터링

--- src/components/Exspense/Expenses.js
+++ src/components/Exspense/Expenses.js
@@ -1,27 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
+import Card from '../UI/Card';
+import ExpenseFilter from './ExpenseFilter';
 
 const Expenses = ({ items }) => {
-  return (
-    <div className="expenses">
-      <ExpenseItem
-        title={items[0].title}
-        price={items[0].price}
-        date={items[0].date}
-      />
+  // 선택된 연도 상태값 관리
+  const [filteredYear, setFilteredYear] = useState(
+    new Date().getFullYear().toString(),
+  );
 
-      <ExpenseItem
-        title={items[1].title}
-        price={items[1].price}
-        date={items[1].date}
-      />
-      <ExpenseItem
-        title={items[2].title}
-        price={items[2].price}
-        date={items[2].date}
-      />
-    </div>
+  // 자식 컴포넌트 ExpenseFilter에 있는 선택 연도를 끌어올리는 함수.
+  const filterChangeHandler = (selectedYear) => {
+    // console.log('Expense: ', selectedYear);
+    setFilteredYear(selectedYear);
+  };
+
+  // ExpenseItem을 동적으로 렌더링하기.
+  // const iterateExpenseItem = () => {
+  // 콜백 함수의 매개값으로 배열의 요소가 하나씩 전달됨.
+  // 콜백 함수는 배열 요소만큼 반복됨
+  // map의 리턴값: 함수가 적용된 각 요소가 담긴 새로운 배열이 리턴됨.
+  //   return items.map((item) => {
+  //     return (
+  //       <ExpenseItem title={item.title} price={item.price} date={item.date} />
+  //     );
+  //   });
+  // };
+
+  return (
+    <Card className="expenses">
+      <ExpenseFilter onChangeFilter={filterChangeHandler} />
+      {items
+        .filter((item) => item.date.getFullYear().toString() === filteredYear)
+        .map((item) => (
+          <ExpenseItem
+            key={item.id}
+            title={item.title}
+            price={item.price}
+            date={item.date}
+          />
+        ))}
+    </Card>
   );
 };
 
