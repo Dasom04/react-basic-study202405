9dc316234cf2d2b32e4cd39ec4760a02b0d1e42f
Dasom04 <slz_952@naver.com>
2024-05-07 16:42:46 +0900

    useContext와 리펙토링

--- src/components/Side Ifect/MainHeader/MainHeader.js
+++ src/components/Side Ifect/MainHeader/MainHeader.js
@@ -2,11 +2,11 @@ import React from 'react';
 import Navigation from '../Navigation/Navidation';
 import classes from './MainHeader.module.css';
 
-const MainHeader = ({ onLogout }) => {
+const MainHeader = () => {
   return (
     <header className={classes['main-header']}>
       <h1>A Typical Page</h1>
-      <Navigation onLogout={onLogout} />
+      <Navigation />
     </header>
   );
 };
--- src/components/Side Ifect/Navigation/Navidation.js
+++ src/components/Side Ifect/Navigation/Navidation.js
@@ -1,27 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import classes from './Navigation.module.css';
 import AuthContext from '../../store/auth-context';
 
-const Navidation = (props) => {
+const Navidation = () => {
+  // AuthContext에서 제공되는 전역 객체를 디스럭처링으로 분해.
+  const { isLoggedIn, onLogout } = useContext(AuthContext);
+
   return (
     <AuthContext.Consumer>
       {(context) => {
         return (
           <nav className={classes.nav}>
             <ul>
-              {context.isLoggedIn && (
+              {isLoggedIn && (
                 <li>
                   <a href="/">Users</a>
                 </li>
               )}
-              {context.isLoggedIn && (
+              {isLoggedIn && (
                 <li>
                   <a href="/">Admin</a>
                 </li>
               )}
-              {context.isLoggedIn && (
+              {isLoggedIn && (
                 <li>
-                  <button onClick={props.onLogout}>Logout</button>
+                  <button onClick={onLogout}>Logout</button>
                 </li>
               )}
             </ul>
