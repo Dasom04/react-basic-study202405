9d455a0381a988e476d52bb9ae761baf120573aa
Dasom04 <slz_952@naver.com>
2024-05-07 15:11:57 +0900

    useReducer로 다중 상태값 중앙관리하기

--- src/components/Side Ifect/Login/Login.js
+++ src/components/Side Ifect/Login/Login.js
@@ -1,13 +1,52 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import Card from '../../UI/Card';
 import styles from './Login.module.css';
 import Button from '../../UI/Button/Button';
+import { type } from '@testing-library/user-event/dist/type';
+
+// 리듀서 함수 선언
+/*
+  이 컴포넌트에서 사용하는 모든 상태와 상태 변경을 중앙 제어하는 함수.
+  컴포넌트 내부 데이터를 사용하지 않고 상태에만 집중하기 때문에
+  컴포넌트 바깥쪽에 선언하는 것이 일반적입니다.
+
+  param1 - state: 변경 전의 상태값
+  param2 - action: dispatch함수(상태 변경 등의 행동)가 전달한 상태 변경 객체
+  return: 관리할 상태값들을 반환
+*/
+
+const emailReducer = (state, action) => {
+  console.log('email reducer called!!');
+  console.log('state: ', state);
+  console.log('action: ', action);
+
+  // dispatch 함수가 전달한 액션 객체의 타입에 딸 변경할 상태 값을 반환.
+  if (action.type === 'USER_INPUT') {
+    return {
+      value: action.val,
+      isValid: action.val.includes('@'),
+    };
+  } else if (action.type === 'INPUT_VALIDATE') {
+    return {
+      value: state.value,
+      isValid: state.value.includes('@'),
+    };
+  }
+};
 
 const Login = ({ onLogin }) => {
-  // 이메일 입력값을 저장
-  const [enteredEmail, setEnteredEmail] = useState('');
-  // 이메일 입력이 정상적인지 확인
-  const [emailIsValid, setEmailIsValid] = useState();
+  // email reducer 사용하기
+  /*
+    param1 - reducer function: 위에서 만든 리듀서 함수
+    param2 - initial state: 초기 상태값
+    return1 - 이메일 관련 상태변수 (상태값)
+    return2 - dispatch함수: 상태를 변경할 수 있는 함수 (변경을 위해 호출하는 함수)
+  */
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: '',
+    isValid: null,
+  });
+
   // 패스워드 입력값을 저장
   const [enteredPassword, setEnteredPassword] = useState('');
   // 패스워드 입력이 정상적인지 확인
@@ -15,7 +54,12 @@ const Login = ({ onLogin }) => {
   // 이메일, 패스워드가 둘 다 동시에 정상적인 상태인지 확인
   const [formIsValid, setFormIsValid] = useState(false);
 
-  // 입력란 (이메일, 비미럱호)을 모두 체크하며 from의 버튼 disavled를 헤재하는
+  // 기존의 email 상태 변수 제거함.
+  // 상태값이 필요하다면, reducer에서 제공되는 상태값을 활용.
+  // emailState에서 isValid 프로퍼티를 디스트럭처링함 (프로퍼티로 바로 사용 x)
+  const { isValid: emailIsValid } = emailState;
+
+  // 입력란 (이메일, 비밀번호)을 모두 체크하며 from의 버튼 disavled를 해재하는
   // 상태변수 fromIsVolid의 사이드 이팩트 차리하는 영역
   useEffect(() => {
     // fromIsVolid의 유효성 검증을 일부러 1초 뒤에 실행 하도록 setTimeout을 사용.
@@ -23,9 +67,7 @@ const Login = ({ onLogin }) => {
 
     const timer = setTimeout(() => {
       console.log('useEffect called on Login.js!');
-      setFormIsValid(
-        enteredEmail.includes('@') && enteredPassword.trim().length > 6,
-      );
+      setFormIsValid(emailIsValid && enteredPassword.trim().length > 6);
     }, 1000);
 
     // cleanup 함수 - 컴포넌트가 업데이트 되거나 없어지기 직전에 실행.
@@ -36,10 +78,16 @@ const Login = ({ onLogin }) => {
     };
 
     // 의존성 배열에 상태변수를 넣어주면 그 상태변수가 바뀔 때 마다 useEffect가 재실행됨.
-  }, [enteredEmail, enteredPassword]);
+  }, [emailIsValid, enteredPassword]);
 
   const emailChangeHandler = (e) => {
-    setEnteredEmail(e.target.value);
+    // reducer의 상태 변경은 dispatch 함수를 통해서 처리
+    // dispatch 함수의 매개값 객체의 key는 정해진 것이 아닌, reducer함수에서 구분하기 위해 붙여주는 이름.
+    // 프로퍼티의 key와 value는 자유롭게 줄 수 있습니다. (정해진게 아님!)
+    dispatchEmail({
+      type: 'USER_INPUT',
+      val: e.target.value,
+    });
   };
 
   const passwordChangeHandler = (e) => {
@@ -47,7 +95,9 @@ const Login = ({ onLogin }) => {
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    dispatchEmail({
+      type: 'INPUT_VALIDATE',
+    });
   };
 
   const validatePasswordHandler = () => {
@@ -56,7 +106,7 @@ const Login = ({ onLogin }) => {
 
   const submitHandler = (e) => {
     e.preventDefault();
-    onLogin(enteredEmail, enteredPassword);
+    onLogin(emailState.value, enteredPassword);
   };
 
   return (
@@ -69,7 +119,7 @@ const Login = ({ onLogin }) => {
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
