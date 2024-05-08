e17a911effc4de3ac4944fda550d275d1336c14c
Dasom04 <slz_952@naver.com>
2024-05-08 11:36:04 +0900

    Food 템플릿 작성 중 (Header button 부분)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import './App.css';
 import React from 'react';
-import AuthContext from './components/store/auth-context';
 import Header from './components/Food/Layout/Header';
 
 const App = () => {
-  const { isLoggedIn } = useContext(AuthContext);
-
   return (
     <>
       <Header />
--- src/components/Food/Layout/Header.js
+++ src/components/Food/Layout/Header.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import styles from './Header.module.scss';
 import mealsImage from '../../../assets/meals.jpg';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = () => {
+  const { header, 'main-image': mainImage } = styles;
   return (
     <>
-      <header className={styles.header}>
+      <header className={header}>
         <h1>ReactMeals</h1>
+        <HeaderCartButton />
         <button>Cart</button>
       </header>
-      <div className={styles['main-image']}>
-        <img
-          src={mealsImage}
-          alt='Looks very delicious meals'
-        />
+      <div className={mainImage}>
+        <img src={mealsImage} alt="Looks very delicious meals" />
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Header;
