b13a7fdd645703ef708b408cf15b2e2df91492a0
Dasom04 <slz_952@naver.com>
2024-05-08 17:17:34 +0900

    Cart 화면 +버튼 처리, Context 로직 수정

--- src/components/Food/Cart/CartItem.js
+++ src/components/Food/Cart/CartItem.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styles from './CartItem.module.scss';
 import { format } from 'prettier';
+import CartContext from '../../store/Cart-context';
 
 const CartItem = ({ cart }) => {
   const { name, price, amount } = cart;
@@ -15,6 +16,15 @@ const CartItem = ({ cart }) => {
 
   const formatPrice = new Intl.NumberFormat('ko-KR').format(price);
 
+  const { addItem } = useContext(CartContext);
+
+  // + 버튼 누르면 무조건 amount는 하나다!
+  const cartAddItemHandler = () => {
+    // addItem은 앞에서 작성한 담기 기능에 사용한 함수.
+    // 지금은 항목 추가가 아닌, 기존 항목의 수량만 하나 늘리려는 것.
+    addItem({ ...cart, amount: 1 });
+  };
+
   return (
     <li className="{cartItem">
       <div>
@@ -26,7 +36,7 @@ const CartItem = ({ cart }) => {
       </div>
       <div className={actions}>
         <button>-</button>
-        <button>+</button>
+        <button onClick={cartAddItemHandler}>+</button>
       </div>
     </li>
   );
