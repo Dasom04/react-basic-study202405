3a141255f4ebb96960e5281562cce7b04d1f109f
Dasom04 <slz_952@naver.com>
2024-05-08 15:40:28 +0900

    Cart Context와 Reducer로 전역 상태 관리, 모달 열고 닫기 처리

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import styles from './Input.module.scss';
 
-const Input = ({ input, label }) => {
+const Input = ({ input, label, onAdd }) => {
+  const amountChangeHamdler = (e) => {
+    onAdd(e.target.value);
+  };
+
   return (
     <div className={styles.input}>
       <label htmlFor={input.id}>{label}</label>
-      <input {...input} />
+      <input {...input} onChange={amountChangeHamdler} />
     </div>
   );
 };
